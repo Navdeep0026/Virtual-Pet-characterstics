563a65a0a40cdd6481a389d8fd7026955bacea4d
Navdeep0026 <navdeepkaur3190@conestogac.on.ca>
2024-05-31 14:54:28 -0400

    Delete constructor
    Contructor with the code error is deleted

--- Virtual Pet characterstics/Program.cs
+++ Virtual Pet characterstics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 // Define the Pet class
 class Pet
@@ -27,3 +28,20 @@ class Pet
         Health = Math.Min(10, Health + 1);
         Console.WriteLine($"{Name} has been fed. Hunger decreased, health increased.");
     }
+
+// Method to play with the pet
+public void Play()
+{
+    Happiness = Math.Min(10, Happiness + 2);
+    Hunger = Math.Min(10, Hunger + 1);
+    Console.WriteLine($"{Name} is happy! Happiness increased, hunger increased.");
+}
+
+// Method to let the pet rest
+public void Rest()
+{
+    Health = Math.Min(10, Health + 2);
+    Happiness = Math.Max(0, Happiness - 1);
+    Console.WriteLine($"{Name} is resting. Health increased, happiness decreased.");
+}
+}
